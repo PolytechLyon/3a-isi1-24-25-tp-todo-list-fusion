b120f345d26eba8b2e6b5a859812434db4b05981
Clément <clement.reniers@etu.univ-lyon1.fr>
2024-12-04 14:31:27 +0100

    TP1

---
+++ script.js
@@ -0,0 +1,43 @@
+const ol = document.getElementById('todo-list');
+const newItemTitle = document.getElementById('new-todo-item-title');
+const addBtn = document.getElementById('new-todo-item-add');
+
+addBtn.addEventListener('click', function() {
+    const newLi = document.createElement('li');
+    const newLiTitle = document.createElement('p');
+    newLiTitle.innerText = newItemTitle.value;
+    newLi.appendChild(newLiTitle);
+
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    editButton.classList.add('edit-todo-item')
+    editButton.addEventListener('click', editItem);
+    newLi.appendChild(editButton);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.classList.add('delete-todo-item')
+    deleteButton.addEventListener('click', deleteItem);
+    newLi.appendChild(deleteButton);
+
+
+    ol.appendChild(newLi);
+    newItemTitle.value = '';
+});
+
+function deleteItem(e) {
+    e.target.parentNode.remove();
+}
+
+function editItem(e) {
+    const title = e.target.parentNode.querySelector('p');
+    title.setAttribute('id', 'editable-item');
+    document.getElementById('new-item').style.display = 'none';
+    document.getElementById('edit-item').style.display = 'block';
+    document.getElementById('edit-todo-item-title').value = title.innerText;
+    document.getElementById('edit-todo-item-confirm').addEventListener('click', function() {
+        document.getElementById('editable-item').innerText = document.getElementById('edit-todo-item-title').value;
+        document.getElementById('new-item').style.display = 'block';
+        document.getElementById('edit-item').style.display = 'none';
+    });
+}
